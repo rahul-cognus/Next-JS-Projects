07a4ceaaca074b822a7b1287914b2a895ebed5f5
Rahul Jangid <rahul.jangid.cognus@gmail.com>
2024-04-25 17:37:31 +0530

    Changes

---
+++ restro-app/src/app/restaurant/page.jsx
@@ -0,0 +1,21 @@
+"use client"
+import React, { useState } from 'react'
+import RestaurantLogin from '../_components/RestaurantLogin'
+import RestaurantSignUp from '../_components/RestaurantSignUp'
+
+const Restaurant = () => {
+    const [login, setLogin] = useState(true)
+    return (
+        <>
+            <div>Restaurant Login / Signup Page</div>
+            {
+                login ? <RestaurantLogin /> : <RestaurantSignUp />
+            }
+            <div>
+                <button onClick={() => setLogin(!login)}>{login ? "Do not have Account? SignUp" : "Already have Account? Login"}</button>
+            </div>
+        </>
+    )
+}
+
+export default Restaurant
